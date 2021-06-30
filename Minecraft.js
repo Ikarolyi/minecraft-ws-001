d83a92573122fe4b963742a1cde4f3fe4ad659d3
Ikarolyi <karolyi.istvan1234@gmail.com>
2021-06-30 13:33:07 +0200

    simple commit

--- Minecraft.js
+++ Minecraft.js
@@ -1,8 +1,20 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3000 });
+const express = require('express')
+
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+  const { Server } = require('ws');
+
+const wss = new Server({ server });
+
 const events = new Array("AgentCommand","AgentCreated","PlayerMessage");
-console.log("Please connect to 127.0.0.1:3000");
-console.log("You can use \"#\" control agent.")
+console.log("You can use \"#\" control agent.");
 wss.on('connection', function connection(ws) {
 	for (var c = 0 ; c < events.length ; c++){
 		ws.send(JSON.stringify({
